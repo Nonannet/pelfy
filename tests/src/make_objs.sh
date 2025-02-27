11a06928cc83a27e0042460256e52f1c0c008735
nicolas <nicolas@dockerhost.lan>
2025-02-27 17:21:46 +0100

    Crosscompile Dockerfile fixed, new test obj files generated

--- tests/src/make_objs.sh
+++ tests/src/make_objs.sh
@@ -7,14 +7,15 @@ if [ $# -lt 2 ]; then
 fi
 
 COMPILER=$1
-OPTIMIZATION_FLAG=$2
+FLAGS=$2
 
 # Check if there are any .c files in the directory
 if ls *.c &> /dev/null; then
     for file in *.c; do
-        obj_file="${file%}_${COMPILER}_${OPTIMIZATION_FLAG}.o"
+        obj_file="${file%}_${COMPILER}_${FLAGS}"
+	obj_file=$(echo "$obj_file" | sed 's/[^a-zA-Z0-9]/_/g' | sed 's/_\+/-/g')
         echo "Compiling $file -> $obj_file with $COMPILER and $OPTIMIZATION_FLAG"
-        $COMPILER -c "$file" $OPTIMIZATION_FLAG -o "$obj_file"
+        $COMPILER -c "$file" $FLAGS -o "../obj/$obj_file.o"
         if [ $? -ne 0 ]; then
             echo "Compilation failed for $file"
             exit 1

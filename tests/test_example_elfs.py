da746926649317fd63ddc28db2ededec0c0f855a
Nicolas Kruse <nicolas@nonan.net>
2025-04-02 21:42:44 +0200

    test extended for iterating section.symbols

--- tests/test_example_elfs.py
+++ tests/test_example_elfs.py
@@ -19,9 +19,16 @@ def test_simple_c() -> None:
         print(elf.code_relocations)
         print('\n')
 
+        section_count = 0
         assert elf.sections
         for section in elf.sections:
             assert known_name(section.description), f"Section type {section.type} for {elf.architecture} in {path} is unknown."
+
+            for sym in section.symbols:
+                assert known_name(sym.info), f"Symbol info {sym.info} for {elf.architecture} in {path} is unknown."
+                section_count += 1
+
+        assert section_count > 2
 
         assert elf.symbols
         for sym in elf.symbols:
